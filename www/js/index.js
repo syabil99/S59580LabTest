2b1da86be4ce43b5c8373a0e2856df2719241efc
syabil99 <103593173+syabil99@users.noreply.github.com>
2022-06-23 10:53:53 +0800

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+var link = crossroad.addRoute("sqlclick",function(){
+
+    // .$(ajax({
+    //     type: type of call,
+    //     url: the address of the service to be call,
+    //     data: any data to be transferred during a call, ‘ ‘ if no data transfered
+    //     cache: false.
+    //     success: function(datareceived){
+    //      //instructions to execute when the ajax call is succeeds
+    //     },
+    //     error: function(){
+    //     //instructions to execute when the ajax call is failed
+    //     }
+    //     }); 
+});
